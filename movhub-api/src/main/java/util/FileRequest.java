1fbb6f029ee51c654eef8fe6904df2f84c956081
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-09-01 18:27:26 +0100

    Added template method in Request

--- movhub-api/src/main/java/util/FileRequest.java
+++ movhub-api/src/main/java/util/FileRequest.java
@@ -1,32 +1,26 @@
 package util;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
  * Created by User01 on 28/08/2017.
  */
-public class FileRequest implements IRequest {
+public class FileRequest extends AbstractRequest {
+
+
     @Override
-    public Iterable<String> getContent(String path) {
+    protected InputStream getStream(String path) {
         String [] parts = path.split("/");
         path = parts[parts.length-1].substring(0, 5);
         path = "search-"+path+".txt";
         System.out.println("PATH:" + path);
         ArrayList<String> res = new ArrayList<>();
-        try(InputStream in = ClassLoader.getSystemResource(path).openStream()){
-             /*
-             * Consumir o Inputstream e adicionar dados ao res
-             */
-            Iterator<String> iter = new IteratorFromReader(in);
-            return () -> iter;
-//            try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    res.add(line);
-//                }
-//            }
+        InputStream in;
+        try{
+            return in = ClassLoader.getSystemResource(path).openStream();
         }catch (IOException e){
             throw new UncheckedIOException(e);
         }
--- movhub-api/src/main/java/util/HttpRequest.java
+++ movhub-api/src/main/java/util/HttpRequest.java
@@ -1,33 +1,23 @@
 package util;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by User01 on 28/08/2017.
  */
-public class HttpRequest  implements IRequest {
+public class HttpRequest  extends AbstractRequest {
 
     @Override
-    public Iterable<String> getContent(String path) {
-        List<String> res = new ArrayList<>();
-        try(InputStream in = new URL(path).openStream()){
-            // Consumir o Inputstream e adicionar ao res
-            Iterator<String> iter = new IteratorFromReader(in);
-            return () -> iter;
-//            try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))){
-//                String line;
-//                while((line = reader.readLine()) != null){
-//                    res.add(line);
-//                }
-//
-//            }
-
+    protected InputStream getStream(String path) {
+        InputStream in;
+        try{
+            return in = new URL(path).openStream();
         }catch (IOException e){
             throw new UncheckedIOException(e);
         }
     }
+
 }
