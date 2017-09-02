ef68ed88f136540cb7eb7628d7e28dec0ad5c1db
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-09-02 16:53:56 +0100

    implemented class Cache

--- movhub-api/src/main/java/util/Cache.java
+++ movhub-api/src/main/java/util/Cache.java
@@ -1,7 +1,24 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 /**
  * Created by Moreira on 02-09-2017.
  */
 public class Cache {
+
+    public static <T,R> Function<T, R> memoize(Function<T,R> src){
+        final Map<T,R> data = new HashMap<>();
+        return key ->{
+            R res = data.get(key);
+            if(data == null){
+                res = src.apply(key);
+                data.put(key, res);
+            }
+            return res;
+        };
+    }
 }
+
