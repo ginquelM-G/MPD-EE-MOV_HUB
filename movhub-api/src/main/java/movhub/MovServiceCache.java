63ddabdc055296008c70bf8a4b8b246008c27c11
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-09-02 16:44:16 +0100

    Begin of CACHE implementation

---
+++ movhub-api/src/main/java/movhub/MovServiceCache.java
@@ -0,0 +1,7 @@
+package movhub;
+
+/**
+ * Created by Moreira on 02-09-2017.
+ */
+public class MovServiceCache {
+}
---
+++ movhub-api/src/main/java/util/Cache.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by Moreira on 02-09-2017.
+ */
+public class Cache {
+}
